51d2c9cecb5bf5777482abac03fe849c465ecb03
Joseph Haig <josephhaig@gmail.com>
2017-12-23 23:27:23 +0000

    Take advantage of features of enumerables

--- lib/elite_universe/galaxy.rb
+++ lib/elite_universe/galaxy.rb
@@ -5,15 +5,13 @@ module EliteUniverse
     include Enumerable
 
     def initialize a, b, c
-      @w = [ a, b, c ]
+      @w = [a, b, c]
     end
 
     def planets
-      p = [ EliteUniverse::Planet.new(*@w)]
-      255.times do
+      255.times.inject([EliteUniverse::Planet.new(*@w)]) do |p|
         p << p[-1].next
       end
-      p
     end
 
     def each &block
--- lib/elite_universe/planet.rb
+++ lib/elite_universe/planet.rb
@@ -28,35 +28,30 @@ module EliteUniverse
     end
 
     def name
-      name = ''
-      v = @w
-      l = v[0] & 64
-      4.times do |i|
-        if i < 3 or l > 0
-          n = ( v[2] >> 8 ) & 31
-          name += CHARS[n]
-        end
-        v = twist v
-      end
-      name.gsub('.', '').capitalize
+      (@w[0] & 64 == 0 ? 2 : 3).times.inject([@w]) do |vs|
+        vs << twist(vs[-1])
+      end.inject('') do |name, v|
+        name += CHARS[(v[2] >> 8) & 31]
+      end.tr('.', '').capitalize
     end
 
     def government
-      GOVS[ gov_n ]
+      GOVS[gov_n]
     end
 
     def economy
-      ECONS[ econ_n ]
+      ECONS[econ_n]
     end
 
     def location
-      [ @w[1] >> 8, @w[0] >> 8 ]
+      [@w[1] >> 8, @w[0] >> 8]
     end
 
     def technology
-      t = ((@w[1] >> 8 ) & 3) + ( econ_n ^ 7 ) + (gov_n >> 1) + 1
-      t += 1 if ((gov_n & 1) == 1)
-      t
+      ((@w[1] >> 8 ) & 3) +
+        (econ_n ^ 7) +
+        (gov_n >> 1) +
+        ((gov_n & 1) == 1 ? 2 : 1)
     end
 
     def next
@@ -64,17 +59,26 @@ module EliteUniverse
     end
 
     private
+
+    def version_at_least version
+      # ... because I want to use new features if possible
+      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new(version)
+    end
+
     def twist arr
-      [ arr[1], arr[2], arr.inject(:+) % 65536 ]
+      [
+        arr[1],
+        arr[2],
+        (version_at_least('2.4.0') ? arr.sum : arr.inject(:+)) % 65536
+      ]
     end
 
     def gov_n
-      ( @w[1] >> 3 ) & 7
+      (@w[1] >> 3) & 7
     end
 
     def econ_n
-      e = ( @w[0] >> 8 ) & 7
-      gov_n <= 1 ? e | 2 : e
+      ((@w[0] >> 8) & 7) | (gov_n <= 1 ? 2 : 0)
     end
   end
 end
